7a6e38d848b2b27c04ac59a9be92a36a155f94e5
538023 <83300538+538023@users.noreply.github.com>
2022-07-12 22:46:45 +0200

    implement base server

--- go.mod
+++ go.mod
@@ -2,4 +2,19 @@ module quairme
 
 go 1.18
 
-require github.com/stianeikeland/go-rpio/v4 v4.6.0 // indirect
+require github.com/briandowns/openweathermap v0.18.0
+
+require (
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/grandcat/zeroconf v1.0.0 // indirect
+	github.com/miekg/dns v1.1.50 // indirect
+	github.com/stianeikeland/go-rpio/v4 v4.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
+	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
+	golang.org/x/tools v0.1.11 // indirect
+	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
+)
+
+replace github.com/briandowns/openweathermap => github.com/538023/openweathermap v0.19.1
--- main.go
+++ main.go
@@ -1,20 +1,176 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strconv"
+	"time"
 
-	"github.com/stianeikeland/go-rpio/v4"
+	"github.com/briandowns/openweathermap"
+	"github.com/grandcat/zeroconf"
 )
 
-func main() {
-	fmt.Println("test")
-	if err := rpio.Open(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+var deviceName string = "QuairMe"
+var aqi int = -1
+var apiKey string = "c769c9c8e3b504df816db5f30fe31c1c"
+var coordinates openweathermap.Coordinates
+
+func setup(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, strconv.Itoa(aqi))
+}
+
+func setupRpi(ssid string, password string, latitude float64, longitude float64) {
+	coordinates = openweathermap.Coordinates{
+		Latitude:  latitude,
+		Longitude: longitude,
+	}
+	saveConfig()
+}
+func readDeviceName() {
+	name, err := os.Hostname()
+	if err != nil {
+		return
+	}
+	deviceName = name
+}
+
+func zeroConf() {
+	server, err := zeroconf.Register(deviceName, "_quairme._tcp", "local.", 80, []string{"txtv=0", "lo=1", "la=2"}, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer server.Shutdown()
+}
+
+func loadConfig() {
+	var savedCoordinates openweathermap.Coordinates = openweathermap.Coordinates{}
+	data, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		return
+	}
+	json.Unmarshal(data, &savedCoordinates)
+	coordinates = savedCoordinates
+}
+
+func saveConfig() {
+	data, _ := json.MarshalIndent(coordinates, "", " ")
+	_ = ioutil.WriteFile("config.json", data, 0644)
+}
+
+func connected() (ok bool) {
+	_, err := http.Get("http://clients3.google.com/generate_204")
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+//func gpioUpdate() {
+//
+//}
+//
+//func gpioLoop() chan struct{} {
+//	err := rpio.Open()
+//	if err != nil {
+//		fmt.Println(err)
+//		os.Exit(1)
+//	}
+//  gpioUpdate()
+//	ticker := time.NewTicker(5 * time.Second)
+//	quit := make(chan struct{})
+//	go func() {
+//		for {
+//			select {
+//			case <-ticker.C:
+//				gpioUpdate()
+//			case <-quit:
+//				rpio.Close()
+//				return
+//			}
+//		}
+//	}()
+//	return quit
+//}
+
+func simulatedGpioUpdate() {
+	fmt.Println(strconv.Itoa(aqi))
+}
+
+func simulatedGpioLoop() chan struct{} {
+	ticker := time.NewTicker(5 * time.Second)
+	simulatedGpioUpdate()
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				simulatedGpioUpdate()
+			case <-quit:
+				return
+			}
+		}
+	}()
+	return quit
+}
+
+func requestAqi() {
+	if !connected() {
+		aqi = -1
+		return
+	}
+	pollution, err := openweathermap.NewPollution(apiKey)
+	if err != nil {
+		return
+	}
+	params := &openweathermap.PollutionParameters{
+		Location: openweathermap.Coordinates{
+			Latitude:  0.0,
+			Longitude: 10.0,
+		},
+		Datetime: "current",
 	}
 
-	defer rpio.Close()
+	err = pollution.PollutionByParams(params)
+	if err != nil {
+		return
+	}
+	if len(pollution.List) == 0 {
+		return
+	}
+	aqi = pollution.List[0].Main.Aqi
+}
 
-	fmt.Print("test")
+func updateTicker() chan struct{} {
+	ticker := time.NewTicker(5 * time.Second)
+	requestAqi()
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				requestAqi()
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+	return quit
+}
+
+func main() {
+	readDeviceName()
+	loadConfig()
+	zeroConf()
+	ticker := updateTicker()
+	defer close(ticker)
+	//gpi := gpioLoop()
+	//defer close(gpi)
+	gpi := simulatedGpioLoop()
+	defer close(gpi)
+	http.HandleFunc("/setup", setup)
+	http.ListenAndServe(":80", nil)
 }
